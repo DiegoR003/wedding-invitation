384faaf59985af4b24cfa526f80342ba9392606b
DiegoR003 <168403198+DiegoR003@users.noreply.github.com>
2026-04-30 20:03:35 -0700

    corrección en routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from './App.jsx'
-import AsistenciaPage from './pages/AsistenciaPage.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
- <React.StrictMode>
+import App from "./App.jsx";
+import AsistenciaPage from "./pages/AsistenciaPage.jsx";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<App />} />
         <Route path="/informacion" element={<App />} />
         <Route path="/asistencia" element={<AsistenciaPage />} />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
